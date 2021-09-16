b103f2812ba131b0dd293d7bc9e7c3f95ddae195
rere950303 <yhwjjang1995@naver.com>
2021-09-16 19:45:45 +0900

    feat: "2021-09-16-첫포스팅" 수정2

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "“개발 공부를 기록하고 공유하기 위한 첫 발걸음”",
+        "title": "개발 공부를 기록하고 공유하기 위한 첫 발걸음",
         "excerpt":"개발 공부를 하면서 기록하는 습관의 중요성을 알게 되었다. 얼마 전에 똑같이 고민했던 문제를 두고 다시 구글링을 해야하는 일이 빈번히 발생하는 것이다. 앞으로 나아가면서 고민하면서 공부한 지식을 기록하여 함께 나누고 기억하고자 블로그를 개설하여 첫 포스팅을 하게 되었다. 쉬더라도 멈추지는 않는 내가 되었으면 하는 바람에 첫 글을 작성한다. 화이팅  ","categories": ["Daily"],
-        "tags": [],
-        "url": "https://rere950303.github.io/daily/%EC%B2%AB%ED%8F%AC%EC%8A%A4%ED%8C%85/",
+        "tags": ["Daily"],
+        "url": "https://rere950303.github.io/daily/firstposting/",
         "teaser": null
       }]
